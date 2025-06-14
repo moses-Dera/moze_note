563109b97b42b862ff829971b929e8036903173c
ndigitals <ndigitals001@gmail.com>
2025-06-14 23:24:22 +0100

    Commented out custom login and logout url paths. They are being handles by the first path with an empty string url

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     # User registration page
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    # path('logout/', views.logoutUser, name='logout'),
+    # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(template_name='users/logged_out.html'), name='logout'),
 ]
+
+#test
